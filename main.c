9cfad2bfb5777be5771943b510c18025a9eeeb05
Maciel <maciel_saints@hotmail.com>
2021-01-20 16:25:42 +0000

    Fase 01

--- main.c
+++ main.c
@@ -1,6 +1,60 @@
+  
+///////////////////////////////////////////////////////////////////////////////
+///             Universidade Federal do Rio Grande do Norte                 ///
+///                 Centro de Ensino Superior do Seridó                     ///
+///               Departamento de Computação e Tecnologia                   ///
+///                  Disciplina DCT1106 -- Programação                      ///
+///      Projeto SIG-Inventory: Um Sistema de Controle de Estoques          ///
+///                Developed by @MacielSaints - Oct, 2020                   ///
+///////////////////////////////////////////////////////////////////////////////
+
 #include <stdio.h>
 
+void telaSobre(void);
+
+
 int main(void) {
-  printf("SIG-Inventory\n");
+  telaSobre();
+
   return 0;
+}
+
+
+
+
+void telaSobre(void) {
+  printf("\033[2J\033[1;1H");
+	printf("\n");
+	printf("█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█\n");
+	printf("█                             █ █ █▀▀ █▀▀█ █▄ █                               █\n");
+	printf("█                             █ █ █▀▀ █ █  █ ▀                                █\n");
+	printf("█                             ▀▀▀ ▀   ▀  ▀ ▀                                  █\n");
+	printf("█               Universidade Federal do Rio Grande do Norte                   █\n");
+	printf("█                   Centro de Ensino Superior do Seridó                       █\n");
+	printf("█                 Departamento de Computação e Tecnologia                     █\n");
+	printf("█         Projeto SIG-Inventory: Um Sistema de Controle de Estoques           █\n");
+	printf("█                   Developed by @MacielSaints - Oct, 2020                    █\n");
+	printf("█                                                                             █\n");
+	printf("▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀\n");
+
+  printf("\n");
+
+  printf("█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█\n");
+  printf("█                   ╔═════════════════════════════════╗                       █\n");
+  printf("█                   ║ Sistema de Controle de Estoques ║                       █\n");
+  printf("█                   ╚═════════════════════════════════╝                       █\n");
+  printf("█    Projeto desenvolvido na disciplina DCT1106 - As empresas da              █\n");
+  printf("█    atualidade utilizam complexos sistemas gerenciais para, entre            █\n");
+  printf("█    várias outras funções, gerenciar seu estoque de produtos.                █\n");
+  printf("█    Gerenciar estoques de maneira eficiente, sem a ajuda de um sistema       █\n");
+  printf("█    automatizado, é algo difícil para as pequenas empresas e impossível      █\n");
+  printf("█    para as grandes. O presente projeto tem como objetivo propor um          █\n");
+  printf("█    software que ofereça funcionalidades básicas relacionadas ao             █\n");
+  printf("█    controle de inventário de uma empresa, visando facilitar a               █\n");
+  printf("█    administração de estoques de produtos dos mais diversos tipos.           █\n");
+  printf("█                                                                             █\n");
+  printf("▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀\n");
+  printf("\n");
+  printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+  getchar();
 }
