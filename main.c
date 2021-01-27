aea167b8fb559d820f56e05c6fb78b27d6653c7d
Maciel <maciel_saints@hotmail.com>
2021-01-27 13:10:31 +0000

    Fase 2

--- main.c
+++ main.c
@@ -9,17 +9,89 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 #include <stdio.h>
+#include <stdlib.h>
 
+char menuInicial(void);
+char telaMenuInicial(void);
 void telaSobre(void);
+char menuPrincipal(void);
+char login (void);
+char novoUsuario (void);
+char sair (void);
 
 
 int main(void) {
-  telaSobre();
 
+		menuInicial();
+  
+	return 0;
+}
+
+char menuInicial(void) {
+	char opcao;
+	do {
+		opcao = telaMenuInicial();
+		switch (opcao) {
+			case '1' : 	login();
+						break;
+			case '2' : 	novoUsuario();
+						break;
+      case '3' : 	telaSobre();
+						break;            
+			case '0' : 	sair();
+						break;
+		}
+	} while (opcao != '0');
   return 0;
 }
 
+char telaMenuInicial(void) {
+	char op;
+  printf("\033[2J\033[1;1H");
+	printf("╔═════════════════════════════════╗\n");
+  printf("║          SIG-Inventory          ║\n");
+  printf("╚═════════════════════════════════╝\n");
 
+  printf("╔══════════════════════════════════╗\n");
+  printf("║ ████████     MENU      █████████ ║\n");
+  printf("║══════════════════════════════════║\n");
+  printf("║          1 - Login               ║\n");
+  printf("║          2 - Novo Usuário        ║\n");
+  printf("║          3 - Sober               ║\n");
+  printf("║          0 - Exit                ║\n");
+  printf("╚══════════════════════════════════╝\n");
+  printf("~>: ");
+  scanf("%c", &op);
+	printf("\n");
+	getchar();
+	return op;
+}
+
+
+char login (void) {
+	printf("\n\n\nLogin.\n\n\n");
+	printf("\n");
+	printf("\n\n\t\t\t\t<<< Em Desenvolvimento >>>\n\n");
+	printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+	getchar();
+  return 0;
+}
+
+char novoUsuario (void) {
+	printf("\n\n\nNovo Usuário\n\n\n");
+	printf("\n");
+	printf("\n\n\t\t\t\t<<< Em Desenvolvimento >>>\n\n");
+	printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+	getchar();
+  return 0;
+}
+
+char sair (void) {
+	printf("\n\n\nAté Logo  õ/.\n\n\n");
+	printf("\n");
+	getchar();
+  return 0;
+}
 
 
 void telaSobre(void) {
